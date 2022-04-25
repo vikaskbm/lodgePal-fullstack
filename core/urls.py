a7bea2d1806300b4b90e4885ca039bff80dfd14e
Vikas Bishnoi <vikaskbm@gmail.com>
2022-04-25 16:46:25 +0530

    create test data and test serializer

---
+++ core/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path 
+from . import views
+
+app_name = 'core'
+urlpatterns = [
+    path('list', views.list_rooms),
+]
--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
 
     avatar = models.ImageField(upload_to="avatars", blank=True)
     superhost = models.BooleanField(default=False)
-    favs = models.ManyToManyField("rooms.Room", related_name="favs")
+    favs = models.ManyToManyField("rooms.Room", related_name="favs", blank=True)
 
     def room_count(self):
         return self.rooms.count()
