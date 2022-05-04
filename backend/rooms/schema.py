efb766ca532459c7dbea39381bbb7e54816bc789
Vikas Bishnoi <vikaskbm@gmail.com>
2022-05-04 19:51:26 +0530

    separate room types

--- backend/rooms/schema.py
+++ backend/rooms/schema.py
@@ -1,21 +1,17 @@
 import graphene
-from graphene_django import DjangoObjectType
+
+from .types import RoomListResponse
+
 from .models import Room
-
-class RoomType(DjangoObjectType):
-    class Meta:
-        model = Room
-
-
-class RoomListResponse(graphene.ObjectType):
-    arr = graphene.List(RoomType)
-    total = graphene.Int()
 
 
 class Query():
     rooms = graphene.Field(RoomListResponse, page=graphene.Int())
 
     def resolve_rooms(self, info, page=1):
+        if page < 0:
+            page = 1
+
         page_size = 5
         start = page_size * (page-1)
         end = page_size * page
---
+++ backend/rooms/types.py
@@ -0,0 +1,12 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import Room
+
+class RoomType(DjangoObjectType):
+    class Meta:
+        model = Room
+
+
+class RoomListResponse(graphene.ObjectType):
+    arr = graphene.List(RoomType)
+    total = graphene.Int()
