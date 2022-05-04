7ca7ca917a74b4fa5308ecbe0929862ae28fe4d7
Vikas Bishnoi <vikaskbm@gmail.com>
2022-05-04 19:55:35 +0530

    get single room

--- backend/rooms/schema.py
+++ backend/rooms/schema.py
@@ -1,12 +1,14 @@
+from typing_extensions import Required
 import graphene
 
-from .types import RoomListResponse
+from .types import RoomListResponse, RoomType
 
 from .models import Room
 
 
 class Query():
     rooms = graphene.Field(RoomListResponse, page=graphene.Int())
+    room = graphene.Field(RoomType, id=graphene.Int(required=True))
 
     def resolve_rooms(self, info, page=1):
         if page < 0:
@@ -19,6 +21,9 @@ class Query():
         arr = Room.objects.all()[start: end]
         total = Room.objects.count()
         return RoomListResponse(arr=arr, total=total)
+        
+    def resolve_room(self, info, id):
+        return Room.objects.get(id=id)
 
 
 class Mutation(graphene.ObjectType):
