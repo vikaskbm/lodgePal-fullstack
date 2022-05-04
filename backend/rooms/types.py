80c7f4531311bab080ae1fe96c2768d64774049f
Vikas Bishnoi <vikaskbm@gmail.com>
2022-05-05 01:17:04 +0530

    add dynamic isfav room

--- backend/rooms/types.py
+++ backend/rooms/types.py
@@ -3,9 +3,17 @@ from graphene_django import DjangoObjectType
 from .models import Room
 
 class RoomType(DjangoObjectType):
+    is_fav = graphene.Boolean()
+
     class Meta:
         model = Room
 
+    def resolve_is_fav(room, info):
+        user = info.context.user
+
+        if user.is_authenticated:
+            return room in user.favs.all()
+        return False
 
 class RoomListResponse(graphene.ObjectType):
     arr = graphene.List(RoomType)
