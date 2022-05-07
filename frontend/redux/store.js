49e6edcd572e31415b2b15d88613a0098bef3c13
Vikas Bishnoi <vikaskbm@gmail.com>
2022-05-07 08:23:09 +0530

    Add async storage in redux store

--- frontend/redux/store.js
+++ frontend/redux/store.js
@@ -1,8 +1,32 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  REHYDRATE,
+  PAUSE,
+  FLUSH,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import rootReducer from "./rootReducer";
+import { AsyncStorage } from "@react-native-async-storage/async-storage";
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [REHYDRATE, PAUSE, FLUSH, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+const persistedStore = persistStore(store);
 
 export default store;
