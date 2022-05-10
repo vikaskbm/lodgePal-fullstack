167588107c168040835198e5c961e90dcfe8c122
Vikas Bishnoi <vikaskbm@gmail.com>
2022-05-10 11:23:36 +0530

    add screens for main application

---
+++ frontend/screens/Main/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components/native";
+
+const Container = styled.View``;
+
+const Text = styled.Text``;
+
+export default () => (
+  <Container>
+    <Text>Profile</Text>
+  </Container>
+);
