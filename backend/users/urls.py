18caa21285a7d991b8e9b2518effbb2d9c7f3fb1
Vikas Bishnoi <vikaskbm@gmail.com>
2022-05-12 15:17:36 +0530

    Create custom permission for current user

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,12 +1,11 @@
+from django.db import router
+from rest_framework.routers import DefaultRouter
+
 from django.urls import path
 from . import views
 
 app_name = "users"
+router = DefaultRouter()
+router.register("", views.UsersViewSet)
 
-urlpatterns = [
-    path("", views.UsersView.as_view()),
-    path("token/", views.login),
-    path("me/", views.MeView.as_view()),
-    path("me/favs/", views.FavsView.as_view()),
-    path("<int:pk>/", views.user_detail)
-]
+urlpatterns = router.urls
--- backend/users/views.py
+++ backend/users/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth import authenticate
 from django.conf import settings
 
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
+from rest_framework.decorators import action
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -14,37 +15,50 @@ import jwt
 from rooms.serializers import RoomSerializer
 from rooms.models import Room
 
+from .permissions import IsSelf
 from .models import User
 from .serializers import UserSerializer
 
 
-class UsersView(APIView):
-    def post(self, request):
-        print(request.data)
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class UsersViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-
-class MeView(APIView):
-    permission_classes = [IsAuthenticated,]
-    
-    def get(self, request):
-        return Response(UserSerializer(request.user).data)
-
-    def put(self, request):
-        serializer = UserSerializer(request.user, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(status=status.HTTP_200_OK)
+    def get_permissions(self):
+        permission_classes = []
+        if self.action == "list":
+            permission_classes = [permissions.IsAdminUser]
+        elif self.action == "create" or self.action == "retrieve":
+            permission_classes = [permissions.AllowAny]
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            permission_classes = [IsSelf | permissions.IsAdminUser]
+
+        return [permission() for permission in permission_classes]
+
+    @action(detail=False, methods = ['post'])
+    def login(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(username=username, password=password)
+        print(user)
+        if user:
+            encoded_jwt = jwt.encode(
+                {"pk": user.pk},
+                settings.SECRET_KEY,
+                algorithm="HS256"
+            )
+            return Response(data={"id": user.id, "token": encoded_jwt}, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+
 
 
 class FavsView(APIView):
-    permission_classes = [IsAuthenticated,]
+    # permission_classes = [IsAuthenticated,]
 
     def get(self, request):
         user = request.user
@@ -66,29 +80,3 @@ class FavsView(APIView):
                 pass
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(["GET"])
-def user_detail(request, pk):
-    user = get_object_or_404(User, pk=pk)
-    if user is not None:
-        return Response(UserSerializer(user).data)
-    return Response(status=status.HTTP_404_NOT_FOUND)
-
-
-@api_view(["POST"])
-def login(request):
-    username = request.data.get("username")
-    password = request.data.get("password")
-    if not username or not password:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    user = authenticate(username=username, password=password)
-    print(user)
-    if user:
-        encoded_jwt = jwt.encode(
-            {"pk": user.pk},
-            settings.SECRET_KEY,
-            algorithm="HS256"
-        )
-        return Response(data={"id": user.id, "token": encoded_jwt}, status=status.HTTP_202_ACCEPTED)
-    else:
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
