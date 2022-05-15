9d3e46ba91319cce796c8c0451cc15cfa52d6088
Vikas Bishnoi <vikaskbm@gmail.com>
2022-05-15 07:35:10 +0530

    add search bar component

--- frontend/screens/Main/Search/SearchPresenter.js
+++ frontend/screens/Main/Search/SearchPresenter.js
@@ -1,16 +1,44 @@
 import React from "react";
 import styled from "styled-components/native";
+import DismissKeyboard from "../../../components/Auth/DismissKeyboard";
+import { useNavigation } from "@react-navigation/native";
 
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
+const Container = styled.View``;
+
+const SearchContainer = styled.View`
+  margin-top: 50px;
+  flex-direction: row;
+  justify-content: space-between;
   align-items: center;
+  padding: 10px 20px;
 `;
 
-const Text = styled.Text``;
+const SearchBar = styled.TextInput`
+  height: 40px;
+  width: 80%;
+  background-color: white;
+  box-shadow: 1px 5px 5px rgba(200, 200, 200, 0.5);
+  border-radius: 12px;
+  justify-content: center;
+  padding-left: 10px;
+`;
 
-export default () => (
-  <Container>
-    <Text>Search</Text>
-  </Container>
-);
+const CancelContainer = styled.TouchableOpacity``;
+
+const CancelText = styled.Text``;
+
+export default () => {
+  const navigation = useNavigation();
+  return (
+    <DismissKeyboard>
+      <Container>
+        <SearchContainer>
+          <SearchBar autoFocus={true} />
+          <CancelContainer onPress={() => navigation.goBack()}>
+            <CancelText>Cancel</CancelText>
+          </CancelContainer>
+        </SearchContainer>
+      </Container>
+    </DismissKeyboard>
+  );
+};
